917502d28e85c7e6c76b0e642d2ab97a9754e7ac
Rakesh Raushan <raushanrakesh069@gmail.com>
2022-09-11 23:29:42 +0530

    Added Dynamic span

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -4,17 +4,20 @@ export default class ImageCard extends Component {
   constructor(props) {
     super(props);
     this.ref = React.createRef();
+    this.state = { span: 0 };
   }
   componentDidMount() {
     this.ref.current.addEventListener("load", this.setSpan);
   }
   setSpan = () => {
-    console.log(this.ref.current.clientHeight);
+    const height = this.ref.current.clientHeight;
+    const span = Math.ceil(height / 150);
+    this.setState({ span });
   };
   render() {
     const { urls, description } = this.props.image;
     return (
-      <div>
+      <div style={{ gridRowEnd: `span ${this.state.span}` }}>
         <img ref={this.ref} src={urls.regular} alt={description} />
       </div>
     );
